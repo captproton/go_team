5efad62f805d0ea4d006ba4fc256671593fc45d7
captproton <carl@wdwhub.net>
2020-03-14 09:24:03 -0700

    ADDED: feature/add_team_invitations

---
+++ app/controllers/teams/team_users_controller.rb
@@ -0,0 +1,26 @@
+class Teams::TeamUsersController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_team
+    def create
+       team_user        = @team.team_users.new(team_user_params) 
+       team_user.team   = @team
+       # TODO: add default roles/default roles here    
+
+       if team_user.save
+           # TODO send email for new project if user already exists
+           redirect_to @team, notice: "Success"
+       else
+           redirect_to @team, alert:  "Failure"
+       end
+    end
+
+    private
+
+    def set_team
+        @team = current_user.teams.find(params[:team_id])
+    end
+
+    def team_user_params
+        params.require(:team_user).permit(:email)
+    end
+end
--- app/models/team_user.rb
+++ app/models/team_user.rb
@@ -1,4 +1,12 @@
 class TeamUser < ApplicationRecord
   belongs_to :team
   belongs_to :user
+
+  attribute :email, :string
+
+  before_validation :set_user_id, if: :email?
+
+  def set_user_id
+    self.user =   User.invite!(email: email)
+  end
 end
