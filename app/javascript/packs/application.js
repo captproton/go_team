546a92ebcbe5ca501c81ab6b80f9451bfece31ce
captproton <carl@wdwhub.net>
2020-03-17 15:50:31 -0700

    CHANGE: UNDO breaking change in application.html.erb and js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,20 +11,6 @@ require("local-time").start();
 
 window.Rails = Rails;
 
-import Vue from "vue/dist/vue.esm";
-
-import TurbolinksAdapter from "vue-turbolinks";
-Vue.use(TurbolinksAdapter);
-
-import App from "../app.vue";
-Vue.component("app", App);
-
-document.addEventListener("turbolinks:load", () => {
-  const app = new Vue({
-    el: '[data-behavior="vue"]'
-  });
-});
-
 import "bootstrap";
 import "data-confirm-modal";
 
